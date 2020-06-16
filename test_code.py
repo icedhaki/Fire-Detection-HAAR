1899e8d0ec441bbea57b1a38e9d665cfbe0e0ba7
Khaki <shyntask@lafayette.edu>
2020-06-16 10:50:57 -0400

    program identifies only large boxes

--- test_code.py
+++ test_code.py
@@ -10,9 +10,10 @@ while True:
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     fires = fire_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in fires:
-        cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-        roi_gray = gray[y:y+h, x:x+w]
-        roi_color = img[y:y+h, x:x+w]
+        if(w>200 & h>100):
+            cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = img[y:y+h, x:x+w]
     cv2.imshow('img',img)
     k = cv2.waitKey(30) & 0xff
     if k == 27:
