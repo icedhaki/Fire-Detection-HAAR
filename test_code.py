d2a16b70e0ff450b6878e0af35b264bba8b83039
Khaki <shyntask@lafayette.edu>
2020-06-16 11:46:20 -0400

    fixed if statement

--- test_code.py
+++ test_code.py
@@ -3,14 +3,16 @@ import cv2
 
 fire_cascade = cv2.CascadeClassifier('cascade.xml')
 
-cap = cv2.VideoCapture(0)
+
+
+cap = cv2.VideoCapture('videoplayback.mp4')
 
 while True:
     ret, img = cap.read()
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     fires = fire_cascade.detectMultiScale(gray, 1.3, 5)
     for (x,y,w,h) in fires:
-        if(w>200 & h>100):
+        if(w>50 and h>50):
             cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
             roi_gray = gray[y:y+h, x:x+w]
             roi_color = img[y:y+h, x:x+w]
